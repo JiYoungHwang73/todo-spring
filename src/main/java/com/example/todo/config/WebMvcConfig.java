b11e84990c8c50dc348167c7a61636f808886eba
HJY <wldud172@gmail.com>
2023-04-06 16:40:40 +0900

    25. 스프링 시큐리티와 JWT 토큰 생성  - build.gradle이 refresh 되지 않아 프로젝트가 인식되지 않는 문제  - [JJWT 라이브러리 설치 에러 :: 경아의 개발 블로그 - https://kyung-a.tistory.com/32]  - build.gradle 수정 후 refresh로 해결 -> 이후 원래 build.gradle로 되돌아가도 오류 없었음.

--- src/main/java/com/example/todo/config/WebMvcConfig.java
+++ src/main/java/com/example/todo/config/WebMvcConfig.java
@@ -5,17 +5,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer{
 	private final long MAX_AGE_SECS = 3600;
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOriginPatterns("*")
-		.allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
-		.allowedHeaders("*")
-		.allowCredentials(true)
-		.maxAge(MAX_AGE_SECS);
+				//.allowedOrigins("http://localhost:3000","http://202.31.139.30:3000")
+				.allowedOriginPatterns("*")
+				.allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(MAX_AGE_SECS);
 	}
-
 }
